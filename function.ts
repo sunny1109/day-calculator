9463cc7e7ae6db5c44186ec02dd8912f7ffb2d65
Le Thi Hue <huehashihama@gmail.com>
2022-04-02 17:08:22 +0900

    eatFruit cho sunny va kota

--- function.ts
+++ function.ts
@@ -13,14 +13,25 @@
 // sleepAtNight();
 // eatBreakfast();
  
-const introduce = (name : string, age : number) => {
-    console.log('toi la ' + name + ', ' + age + ' tuoi');
-    console.log('toi khoe');
-};
-introduce('sunny', 18);
-introduce('kota', 40);
-introduce('masaki', 35);
+// const introduce = (name : string, age : number) => {
+//     console.log('toi la ' + name + ', ' + age + ' tuoi');
+//     console.log('toi khoe');
+// };
+// introduce('sunny', 18);
+// introduce('kota', 40);
+// introduce('masaki', 35);
  
+const eatFruit = (name : string) => {
+    
+    const amount = 3;
+    console.log('banana ' + amount);
+    console.log('tamago');
+    console.log('ninjin');
+    console.log('mango');
+    console.log(name + ' da an');
+}
+eatFruit('kota');
+eatFruit('sunny');
 
 
 
