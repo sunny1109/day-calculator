2284b039aaa0155cc72dad7fa8e0ae543fd2bf51
Le Thi Hue <huehashihama@gmail.com>
2022-04-02 15:53:29 +0900

    first commit

---
+++ function.ts
@@ -0,0 +1,26 @@
+//  const sleepAtNight = () => {
+//     console.log('ăn kem');
+//     console.log('nằm xuống');
+//     console.log('ngủ');
+// };
+
+// const eatBreakfast = () => {
+//     console.log('lay keo');
+//     console.log('boc keo');
+//     console.log('an keo');
+// };
+// eatBreakfast();
+// sleepAtNight();
+// eatBreakfast();
+ 
+const introduce = (name : string, age : number) => {
+    console.log('toi la ' + name + ', ' + age + ' tuoi');
+    console.log('toi khoe');
+};
+introduce('sunny', 18);
+introduce('kota', 40);
+introduce('masaki', 35);
+ 
+
+
+
---
+++ index.ts
@@ -0,0 +1,9 @@
+const ar = 'hihi';
+console.log ( ar + ' sunny');
+
+let aa = 'xin chao';
+console.log ( aa + ' lu');
+
+
+
+console.log('hello world');
