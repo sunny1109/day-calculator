11ee4f84cddccbedea1c9669a8652477c9093b10
Le Thi Hue <huehashihama@gmail.com>
2022-04-02 18:46:27 +0900

    javascript renshuu

---
+++ date.ts
@@ -0,0 +1,16 @@
+// const date = new Date('2022-04-02 18:01');
+// console.log(date);
+// let year =date.getFullYear();
+// console.log(year);
+// let day = date.getDate();
+// console.log(day); 
+// let month = date.getMonth() + 1;
+// console.log(month);
+
+// const allyear = year + '年' + month + '月' + day + '日'
+// console.log(allyear);
+// console.log(date.getHours());
+
+// console.log(String(date.getMinutes()).padStart(2, '0'));
+// console.log(allyear + date.getHours() + ':' + String(date.getMinutes()).padStart(2, '0'));
+
